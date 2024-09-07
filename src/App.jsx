04f4ac4e2212067c7b032a86230d745b21620980
Hrushikesh Pathak <hrushikeshpathak@Hrushikeshs-MacBook-Pro.local>
2024-09-07 18:00:47 +0530

    landing page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ChairmanMessage from './Pages/Chairmanmessgae'
 import FinancialPerformance from './Pages/FinincalPerfomance'
 import Quality from './Pages/Quality'
