fe48738ad5f4479e5a5b82c6ca82f193c81ff634
Hrushikesh Pathak <hrushikeshpathak@Hrushikeshs-MacBook-Pro.local>
2024-09-07 18:22:10 +0530

    landing page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ChairmanMessage from './Pages/Chairmanmessgae'
-import FinancialPerformance from './Pages/FinincalPerfomance'
-import Quality from './Pages/Quality'
-import Awards from './Pages/Awards'
-import Engineering from './Pages/Engineering'
-import OperatingLandScape from './Pages/OperatingLandScape'
-import Operations from './Pages/Operations'
-import Test from './Pages/Test';
-import DistinguishClients from './Pages/DistinguishClients'
-import Excellence from './Pages/Excellence'
-import EngineeringFutureStakeholder from './Pages/EngineeringFutureStakeholder'
-import AcceleratingGrowth from './Pages/AcceleratingGrowth'
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Pages/Home'
 import Footer from './Components/Footer/Footer'
+import FinancialPerformance from './Pages/FinincalPerfomance';
+import AcceleratingGrowth from './Pages/AcceleratingGrowth';
+import ChairmanMessage from './Pages/Chairmanmessgae';
+import Excellence from './Pages/Excellence';
+import EngineeringFutureStakeholder from './Pages/EngineeringFutureStakeholder';
+import Engineering from './Pages/Engineering';
+import OperatingLandScape from './Pages/OperatingLandScape';
+import Operations from './Pages/Operations';
+import DistinguishClients from './Pages/DistinguishClients';
+import Quality from './Pages/Quality';
+import Awards from './Pages/Awards';
 
 function App() {
 
   return (
     <div>
-      <Navbar/>
-      <Home/>
-    {/* <ChairmanMessage/> */}
-    {/* <FinancialPerformance/> */}
-    {/* <Quality/>
-    <Awards/> */}
-    {/* <Engineering/> */}
-    {/* <OperatingLandScape/> */}
-    {/* <OperatingLandScape/> */}
-    {/* <Operations/> */}
-    {/* <Test/> */}
-    <Footer/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/financial' element={<FinancialPerformance />}/>
+        <Route path='/accelerating-growth' element={<AcceleratingGrowth />}/>
+        <Route path='/management-message' element={<ChairmanMessage />}/>
+        <Route path='/excellence' element={<Excellence />}/>
+        <Route path='/engineering-future-stakeholder' element={<EngineeringFutureStakeholder />}/>
+        <Route path='/engineering-excellence' element={<Engineering />}/>
+        <Route path='/operating-landscape' element={<OperatingLandScape />}/>
+        <Route path='/operations' element={<Operations />}/>
+        <Route path='/distinguished-clients' element={<DistinguishClients />}/>
+        <Route path='/quality' element={<Quality />}/>
+        <Route path='/awards' element={<Awards />}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Navbar.css'; // Make sure to add your CSS here
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [isActive, setIsActive] = useState(false);
@@ -8,26 +9,19 @@ const Navbar = () => {
         setIsActive(!isActive);
     };
 
+    const closeMenu = () => {
+        setIsActive(false); // Close the menu when any link is clicked
+    };
+
     return (
         <div className={`hero ${isActive ? 'is-active' : ''}`}>
-            {/* <svg xmlns="http://www.w3.org/2000/svg" style={{ display: 'none' }}>
-                <defs>
-                    <symbol viewBox="0 0 60 60" height={"10px"} width={"60"} id="icon-download">
-                        <path d="M30 0C13.4 0 0 13.4 0 30s13.4 30 30 30 30-13.4 30-30S46.6 0 30 0zm0 56C16.5 56 4 43.5 4 30S16.5 4 30 4s26 12.5 26 26-12.5 26-26 26zm-5-26V15h10v15h8l-13 13-13-13h8z" />
-
-                    </symbol>
-
-
-                </defs>
-            </svg> */}
-
             <header id="masthead" role="banner" className={isActive ? 'is-active' : ''}>
                 <div className="n-container">
-
                     <div id="masthead-search">
-                        <a href="/"><img src="./homepage/pitti-logo.jpg" alt="" /></a>
+                        <Link to="/" onClick={closeMenu}>
+                            <img src="./homepage/pitti-logo.jpg" alt="" />
+                        </Link>
                     </div>
-
                     <button
                         className={`hamburger hamburger--boring ${isActive ? 'is-active' : ''}`}
                         type="button"
@@ -38,70 +32,55 @@ const Navbar = () => {
                         </span>
                         <span className="hamburger-label">Menu</span>
                     </button>
-
-
-
                     <nav id="site-nav" role="navigation" className={isActive ? 'is-active' : ''}>
                         <div className="col">
                             <h4>Corporate Overview</h4>
                             <ul>
-                                <li><a href="#">Board of Directors</a></li>
-                                <li><a href="#">About Pitti</a></li>
-                                <li><a href="#">Wide Array of Offerings</a></li>
-                                <li><a href="#">Financial Performance</a></li>
-                                <li><a href="#">Accelerating growth inorganically </a></li>
-                                <li><a href="#">Management’s Message</a></li>
-                                <li><a href="#">Excellence Has No Borders</a></li>
-                                <li><a href="#">Engineering the Future with Decades of Excellence</a></li>
-                                <li><a href="#">Engineering the Future with Key Stakeholders</a></li>
-                                <li><a href="#">Operating Landscape</a></li>
-                                <li><a href="#">Operations </a></li>
-                                <li><a href="#">Distinguished Clients</a></li>
-                                <li><a href="#">Quality</a></li>
-                                <li><a href="#">Awards and Accolades</a></li>
+                                <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+                                <li><Link to="/financial" onClick={closeMenu}>Financial Performance</Link></li>
+                                <li><Link to="/accelerating-growth" onClick={closeMenu}>Accelerating growth inorganically</Link></li>
+                                <li><Link to="/management-message" onClick={closeMenu}>Management’s Message</Link></li>
+                                <li><Link to="/excellence" onClick={closeMenu}>Excellence Has No Borders</Link></li>
+                                <li><Link to="/engineering-future-stakeholder" onClick={closeMenu}>Engineering the Future with Decades of Excellence</Link></li>
+                                <li><Link to="/engineering-excellence" onClick={closeMenu}>Engineering the Future with Key Stakeholders</Link></li>
+                                <li><Link to="/operating-landscape" onClick={closeMenu}>Operating Landscape</Link></li>
+                                <li><Link to="/operations" onClick={closeMenu}>Operations</Link></li>
+                                <li><Link to="/distinguished-clients" onClick={closeMenu}>Distinguished Clients</Link></li>
+                                <li><Link to="/quality" onClick={closeMenu}>Quality</Link></li>
+                                <li><Link to="/awards" onClick={closeMenu}>Awards and Accolades</Link></li>
                             </ul>
                         </div>
-
                         <div className="col">
                             <h4>Statutory Reports</h4>
                             <ul>
-                                <li><a href="#">Management Discussion and Analysis</a></li>
-                                <li><a href="#">Directors’ Report</a></li>
-                                <li><a href="#">Business Responsibility & Sustainability Report</a></li>
-                                <li><a href="#">Report on Corporate Governance</a></li>
+                                <li><Link to="#" onClick={closeMenu}>Management Discussion and Analysis</Link></li>
+                                <li><Link to="#" onClick={closeMenu}>Directors’ Report</Link></li>
+                                <li><Link to="#" onClick={closeMenu}>Business Responsibility & Sustainability Report</Link></li>
+                                <li><Link to="#" onClick={closeMenu}>Report on Corporate Governance</Link></li>
                             </ul>
                         </div>
-
                         <div className="col">
                             <h4>Financial Statements</h4>
                             <ul>
-                                <li><a href="#">Standalone Financial Statements</a></li>
-                                <li><a href="#">Consolidated Financial Statements</a></li>
+                                <li><Link to="#" onClick={closeMenu}>Standalone Financial Statements</Link></li>
+                                <li><Link to="#" onClick={closeMenu}>Consolidated Financial Statements</Link></li>
                             </ul>
                         </div>
-
                         <div className="col">
                             <h4>Notice</h4>
                             <ul>
-                                <li><a href="#">40th Annual General Meeting</a></li>
+                                <li><Link to="#" onClick={closeMenu}>40th Annual General Meeting</Link></li>
                             </ul>
                         </div>
-
                         <div className="col">
                             <ul className="social">
                                 <li>
-
-                                    {/* <a href="#">
-                                        <svg title="LinkedIn">
-                                            <use xlinkHref="#icon-download" />
-                                        </svg>
-                                    </a> */}
-                                    <a href="#" class="download-button">
+                                    <Link to="#" className="download-button" onClick={closeMenu}>
                                         <div className='svg'>
-                                    <img src="./homepage/download-pdf.svg" alt="" />
+                                            <img src="./homepage/download-pdf.svg" alt="" />
                                         </div>
                                         Download Full Report
-                                    </a>
+                                    </Link>
                                 </li>
                             </ul>
                         </div>
